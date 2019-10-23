523f13e1a26fbe7d1a543a57b404d62c68b7f39d
kurniad-sign <agungkurniadi99@gmail.com>
2019-10-23 19:19:48 +0700

    Updates

---
+++ precache-manifest.43386753463c71b90412744a0d4ee26d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ee98af16c1447dad95a9bebeaa887e",
+    "url": "kurniad-sign.github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ab5d6a12875529a06df6",
+    "url": "kurniad-sign.github.io/monster-rolodex/static/css/main.9618fc9a.chunk.css"
+  },
+  {
+    "revision": "a20f2964f1e7103d046d",
+    "url": "kurniad-sign.github.io/monster-rolodex/static/js/2.3860c490.chunk.js"
+  },
+  {
+    "revision": "ab5d6a12875529a06df6",
+    "url": "kurniad-sign.github.io/monster-rolodex/static/js/main.0d47162a.chunk.js"
+  },
+  {
+    "revision": "e57f29053ef47fa0a712",
+    "url": "kurniad-sign.github.io/monster-rolodex/static/js/runtime-main.49302163.js"
+  }
+]);
